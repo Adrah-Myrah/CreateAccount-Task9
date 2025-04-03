f76d58bc820aec53aeb877e869e383931df6b2f3
JosephineBira <josephinebira3@gmail.com>
2025-04-03 07:27:40 +0100

    commit 5 #
    In nv,i changed the width to 1500 and height 900

--- CreateAccountSwing/CreateAccount_Page/src/main/java/AccountPackage/Login_SignUp.java
+++ CreateAccountSwing/CreateAccount_Page/src/main/java/AccountPackage/Login_SignUp.java
@@ -21,7 +21,7 @@ public class Login_SignUp {
     public JFrame Log(){
         mainFrame = new JFrame("Welcome");
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainFrame.setSize(new Dimension(1000,800));
+        mainFrame.setSize(new Dimension(1500,900));
         mainFrame.setLocationRelativeTo(null);
         mainFrame.add(this.Panel1());
         mainFrame.setVisible(true);
